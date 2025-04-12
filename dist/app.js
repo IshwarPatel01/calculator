ad54c2874c1d2d3d696177a5ef7ee50140465158
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-04-12 16:59:47 +0530

    add html structure

---
+++ dist/app.js
@@ -0,0 +1,22 @@
+const displayInput = document.querySelector("input")
+const btns = document.querySelectorAll(".btn")
+
+
+// console.log(displayInput)
+
+// console.log(btns.length)
+
+// btns.forEach(btn => {
+//     btn.addEventListener("click", () => {
+//         displayInput.value += btn.textContent
+//     })
+// })
+
+
+// algo
+/*
+1. create ui then write algo it will help at best
+2. then think about which function and how many function you will create
+
+
+*/
